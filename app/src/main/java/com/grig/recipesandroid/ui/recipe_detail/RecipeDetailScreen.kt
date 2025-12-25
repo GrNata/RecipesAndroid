0ae739c2812e1be8e53f044d9eff49ce66363b6e
natalagrigoreva <gr.natalij@gmail.com>
2025-12-25 16:44:05 +0300

    TopAppBar с кнопкой «назад»

--- app/src/main/java/com/grig/recipesandroid/ui/recipe_detail/RecipeDetailScreen.kt
+++ app/src/main/java/com/grig/recipesandroid/ui/recipe_detail/RecipeDetailScreen.kt
@@ -7,29 +7,35 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecipeDetailScreen(
     recipeId: Long,
@@ -45,105 +51,132 @@ fun RecipeDetailScreen(
         viewModel.loadRecipe(recipeId)
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = recipe?.name ?: "Рецепт",
+                        style = MaterialTheme.typography.titleLarge,
+//                        color = Color(0xFF9A663B)
+                        color = Color(0xFF245C5C)
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Назад"
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFFEEE2DC)
+                )
+            )
+        }
+    ) { paddingValues ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+            .padding(paddingValues)
 //            .background(Color(0xFFEEE2DC))
 //            .background(Color(0xFFF6ECD7))
 //            .background(Color(0xFFF6E5D7))
-            .background(Color(0xFFEEE2DC))
-    ) {
-        when {
-            loading -> {
-                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-            }
-            error != null -> {
-                Text(
-                    text = error ?: "Ошибка",
-                    modifier = Modifier.align(Alignment.Center),
+                .background(Color(0xFFEEE2DC))
+        ) {
+            when {
+                loading -> {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
+                error != null -> {
+                    Text(
+                        text = error ?: "Ошибка",
+                        modifier = Modifier.align(Alignment.Center),
 //                    color = Color(0xFFC40C4A)
-                    color = Color(0xFF9A3B3B)
-                )
-            }
-            recipe != null -> {
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp)
-                ) {
-                    item {
-                        Text(
-                            requireNotNull(recipe).description ?: "",
-                                modifier = Modifier.padding(bottom = 8.dp, top = 16.dp),
+                        color = Color(0xFF9A3B3B)
+                    )
+                }
+                recipe != null -> {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    ) {
+                        item {
+                            Text(
+                                requireNotNull(recipe).description ?: "",
+                                modifier = Modifier.padding(bottom = 8.dp),
 //                            color = Color(0xFF701332),
-                            color = Color(0xFF9A3B3B),
-                            style = MaterialTheme.typography.titleLarge
-                        )
-                        Row(modifier = Modifier.padding(6.dp)) {
+                                color = Color(0xFF9A3B3B),
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                            Row(modifier = Modifier.padding(6.dp)) {
 
-                            requireNotNull(recipe).image?.let { imageUrl ->
-                                AsyncImage(
-                                    model = imageUrl,
-                                    contentDescription = requireNotNull(recipe).name,
-                                    modifier = Modifier
+                                requireNotNull(recipe).image?.let { imageUrl ->
+                                    AsyncImage(
+                                        model = imageUrl,
+                                        contentDescription = requireNotNull(recipe).name,
+                                        modifier = Modifier
 //                                        .fillMaxWidth()
-                                        .height(180.dp)
-                                        .padding(bottom = 8.dp)
-                                        .clip(RoundedCornerShape(8.dp))
-                                )
-                            }
-//                        Text(
-//                            text = requireNotNull(recipe).description ?: "",
-//                            modifier = Modifier.padding(bottom = 8.dp)
-//                        )
-                            Column() {
-                                Text(
-                                    text = "Категория: ${requireNotNull(recipe).categories.joinToString { it.name.lowercase() }}",
+                                            .height(180.dp)
+                                            .padding(bottom = 8.dp)
+                                            .clip(RoundedCornerShape(8.dp))
+                                    )
+                                }
+                                Column() {
+                                    Text(
+                                        text = "Категория: ${requireNotNull(recipe).categories.joinToString { it.name.lowercase() }}",
 //                            color = Color(0xFF8B8789),
 //                            color = Color(0xFFB29393),
-                                    color = Color(0xFF7E889F),
-                                    textAlign = TextAlign.Center,
-                                    style = MaterialTheme.typography.bodyLarge,
-                                    modifier = Modifier.padding(start = 16.dp)
-                                )
-                                Spacer(modifier = Modifier.height(8.dp))
-                                Text(
-                                    text = "Ингредиенты:",
-                                    color = Color(0xFF656A77),
-                                    style = MaterialTheme.typography.bodyLarge,
-                                    modifier = Modifier.padding(start = 16.dp)
-                                )
-                                requireNotNull(recipe).ingredients.forEach { ing ->
-                                    Text(
-                                        text = "- ${ing.ingredient.name.lowercase()}: ${ing.amount ?: ""} ${ing.unit}",
-                                        color = Color(0xFF656A77),
-                                        style = MaterialTheme.typography.bodyMedium,
+                                        color = Color(0xFF7E889F),
+                                        textAlign = TextAlign.Center,
+                                        style = MaterialTheme.typography.bodyLarge,
                                         modifier = Modifier.padding(start = 16.dp)
                                     )
+                                    Spacer(modifier = Modifier.height(12.dp))
+                                    Text(
+                                        text = "Ингредиенты:",
+                                        color = Color(0xFF656A77),
+                                        style = MaterialTheme.typography.bodyLarge,
+                                        modifier = Modifier.padding(start = 16.dp)
+                                    )
+                                    requireNotNull(recipe).ingredients.forEach { ing ->
+                                        Text(
+                                            text = "- ${ing.ingredient.name.lowercase()}: ${ing.amount ?: ""} ${ing.unit}",
+                                            color = Color(0xFF656A77),
+                                            style = MaterialTheme.typography.bodyMedium,
+                                            modifier = Modifier.padding(start = 16.dp)
+                                        )
 
+                                    }
                                 }
+
                             }
 
-                        }
-
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(
-                            text = "Шаги приготовления:",
-                            color = Color(0xFF123C69),
-                            style = MaterialTheme.typography.bodyLarge,
-                            modifier = Modifier.padding(bottom = 8.dp)
-                        )
-                        requireNotNull(recipe).steps.forEach { step ->
+                            Spacer(modifier = Modifier.height(8.dp))
                             Text(
-                                text = "- ${step}",
-                                modifier = Modifier.padding(bottom = 4.dp),
+                                text = "Шаги приготовления:",
                                 color = Color(0xFF123C69),
-                                style = MaterialTheme.typography.bodyMedium
+                                style = MaterialTheme.typography.bodyLarge,
+                                modifier = Modifier.padding(bottom = 8.dp)
+                            )
+                            requireNotNull(recipe).steps.forEach { step ->
+                                Text(
+                                    text = "- ${step}",
+                                    modifier = Modifier.padding(bottom = 4.dp),
+                                    color = Color(0xFF123C69),
+                                    style = MaterialTheme.typography.bodyMedium
                                 )
+                            }
                         }
                     }
                 }
             }
         }
+
+
     }
+
 }
